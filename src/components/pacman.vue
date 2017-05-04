cb9a1f8ecc87029405c16d3eb67dad09d6821e7f
vinsondragon <vinsondragon@gmail.com>
2017-05-04 21:48:42 +0800

    update

---
+++ src/components/pacman.vue
@@ -0,0 +1,165 @@
+<template>
+    <div class="loader-inner pacman">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+</template>
+<script>
+export default {
+    name: "pacman",
+}
+</script>
+<style lang="scss">
+@-webkit-keyframes rotate_pacman_half_up {
+    0% {
+        -webkit-transform: rotate(270deg);
+        transform: rotate(270deg);
+    }
+    50% {
+        -webkit-transform: rotate(360deg);
+        transform: rotate(360deg);
+    }
+    100% {
+        -webkit-transform: rotate(270deg);
+        transform: rotate(270deg);
+    }
+}
+
+@keyframes rotate_pacman_half_up {
+    0% {
+        -webkit-transform: rotate(270deg);
+        transform: rotate(270deg);
+    }
+    50% {
+        -webkit-transform: rotate(360deg);
+        transform: rotate(360deg);
+    }
+    100% {
+        -webkit-transform: rotate(270deg);
+        transform: rotate(270deg);
+    }
+}
+
+@-webkit-keyframes rotate_pacman_half_down {
+    0% {
+        -webkit-transform: rotate(90deg);
+        transform: rotate(90deg);
+    }
+    50% {
+        -webkit-transform: rotate(0deg);
+        transform: rotate(0deg);
+    }
+    100% {
+        -webkit-transform: rotate(90deg);
+        transform: rotate(90deg);
+    }
+}
+
+@keyframes rotate_pacman_half_down {
+    0% {
+        -webkit-transform: rotate(90deg);
+        transform: rotate(90deg);
+    }
+    50% {
+        -webkit-transform: rotate(0deg);
+        transform: rotate(0deg);
+    }
+    100% {
+        -webkit-transform: rotate(90deg);
+        transform: rotate(90deg);
+    }
+}
+
+@-webkit-keyframes pacman-balls {
+    75% {
+        opacity: 0.7;
+    }
+    100% {
+        -webkit-transform: translate(-100px, -5.25px);
+        transform: translate(-100px, -5.25px);
+    }
+}
+
+@keyframes pacman-balls {
+    75% {
+        opacity: 0.7;
+    }
+    100% {
+        -webkit-transform: translate(-100px, -5.25px);
+        transform: translate(-100px, -5.25px);
+    }
+}
+
+.pacman {
+    left: 50%;
+    margin: 25% auto .5rem;
+    position: relative;
+}
+
+.pacman>div:nth-child(2) {
+    -webkit-animation: pacman-balls 1s -0.99s infinite linear;
+    animation: pacman-balls 1s -0.99s infinite linear;
+}
+
+.pacman>div:nth-child(3) {
+    -webkit-animation: pacman-balls 1s -0.66s infinite linear;
+    animation: pacman-balls 1s -0.66s infinite linear;
+}
+
+.pacman>div:nth-child(4) {
+    -webkit-animation: pacman-balls 1s -0.33s infinite linear;
+    animation: pacman-balls 1s -0.33s infinite linear;
+}
+
+.pacman>div:nth-child(5) {
+    -webkit-animation: pacman-balls 1s 0s infinite linear;
+    animation: pacman-balls 1s 0s infinite linear;
+}
+
+.pacman>div:first-of-type {
+    width: 0px;
+    height: 0px;
+    border-right: 15px solid transparent;
+    border-top: 15px solid rgba(194, 233, 251, 1);
+    border-left: 15px solid rgba(194, 233, 251, 1);
+    border-bottom: 15px solid rgba(194, 233, 251, 1);
+    border-radius: 15px;
+    -webkit-animation: rotate_pacman_half_up 0.5s 0s infinite;
+    animation: rotate_pacman_half_up 0.5s 0s infinite;
+    position: relative;
+    left: -30px;
+}
+
+.pacman>div:nth-child(2) {
+    width: 0px;
+    height: 0px;
+    border-right: 15px solid transparent;
+    border-top: 15px solid rgba(194, 233, 251, 1);
+    border-left: 15px solid rgba(194, 233, 251, 1);
+    border-bottom: 15px solid rgba(194, 233, 251, 1);
+    border-radius: 15px;
+    -webkit-animation: rotate_pacman_half_down 0.5s 0s infinite;
+    animation: rotate_pacman_half_down 0.5s 0s infinite;
+    margin-top: -30px;
+    position: relative;
+    left: -30px;
+}
+
+.pacman>div:nth-child(3),
+.pacman>div:nth-child(4),
+.pacman>div:nth-child(5),
+.pacman>div:nth-child(6) {
+    background-color: rgba(194, 233, 251, 1);
+    border-radius: 100%;
+    width: 5px;
+    height: 5px;
+    position: absolute;
+    -webkit-transform: translate(0, -5.25px);
+    transform: translate(0, -5.25px);
+    top: 18px;
+    left: 70px;
+}
+</style>
---
+++ src/components/scroll.vue
@@ -0,0 +1,50 @@
+<template>
+    <a class="backtoTop" @click="backtoTop()"><i class="iconfont icon-rocket"></i></a>
+</template>
+
+<script>
+export default {
+    name: 'scroll',
+    mounted: function () {
+        this.$nextTick(function () {
+            document.querySelector('.app-view').addEventListener('scroll', this.needtoTop);
+        })
+    },
+    methods: {
+        backtoTop() {
+            document.querySelector('.app-view').scrollTop = 0;
+        },
+        needtoTop: function () {
+            let curHeight = document.querySelector('.app-view').scrollTop;
+            let viewHeight = document.documentElement.clientHeight;
+            if (curHeight > viewHeight) {
+                this.$store.commit('topShow', 1);
+            } else {
+                this.$store.commit('topShow');
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.backtoTop {
+    position: fixed;
+    right: 5%;
+    bottom: 5%;
+    z-index: 3;
+    background: #fff;
+    border-radius: 50%;
+    height: 1rem;
+    width: 1rem;
+    border: none;
+    box-shadow: 0 2px 3px 0 rgba(0, 0, 0, 0.50);
+    i {
+        font-size: 0.5rem;
+        position: absolute;
+        top: 52%;
+        transform: translate(-50%, -50%);
+        left: 52%;
+    }
+}
+</style>
